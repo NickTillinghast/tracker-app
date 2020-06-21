0a1613c21e530e56dbd4f1795fae08ea3fb6361a
Nick Tillinghast <nickt2@cox.net>
2020-06-20 22:24:14 -0700

    first commit -1

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes');
+
+const app = express();
+
+
+app.use(bodyParser.json());
+// parser must be before use auth routes 
+
+app.use(authRoutes);
+
+const mongoUri = 'mongodb+srv://NickTillinghast:kinley082314@cluster0-hfig5.mongodb.net/<dbname>?retryWrites=true&w=majority';
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+})
+mongoose.connection.on('connected', () => {
+    console.log('Connected to mongo instance');
+});
+mongoose.connection.on('error', (err) => {
+    console.log('Error connecting to mongo', err)
+})
+
+app.get('/', (req, res) => {
+    res.send('Hi There!');
+
+});
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+})
